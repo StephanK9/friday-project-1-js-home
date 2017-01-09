bd2955e392b5a8b65c62d73912b5a9554eb562f9
StephanK9 <stephane.khoury@gmail.com>
2017-01-09 12:09:08 -0800

    clean up doctor-interface before starting the refactoring process

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -7,10 +7,8 @@ $(document).ready(function(){
 
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response){
       $('.showDoctors').append("<li>" + response.profile + "</li>");
-    })
-
-    .fail(function(error){
+    }).fail(function(error){
       $('.showDoctors').text(error.responseJSON.message);
-      });
+    });
   });
 });
