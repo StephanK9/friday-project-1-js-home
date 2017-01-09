febdb7e6f1b6ac3277df875c1420e9544d1d36b2
StephanK9 <stephane.khoury@gmail.com>
2017-01-09 13:02:11 -0800

    add handling for asynchronicity, getting undefined error in console.log

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,21 +1,14 @@
-var apiKey = require('./../.env').apiKey;
+
 
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function(){
   var currentDoctorObject = new Doctor();
-  currentDoctorObject.getDoctor();
-})
-
-$(document).ready(function(){
   $('#searchDoctor').click(function(){
     var medicalIssue = $('#symptom').val();
     $('#symptom').val("");
-
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response){
-      $('.showDoctors').append("<li>" + response.profile + "</li>");
-    }).fail(function(error){
-      $('.showDoctors').text(error.responseJSON.message);
-    });
+    var doctor = currentDoctorObject.getDoctor(medicalIssue);
+    $('.showDoctors').text("The doctors for your symptom: " + medicalIssue + " are " + doctor + "%");
+    console.log(doctor);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,8 +1,14 @@
+var apiKey = require('./../.env').apiKey;
+
 function Doctor(){
 }
 
-Doctor.prototype.getDoctor = function(){
-  console.log("Hi! I'm your doctor object.");
+Doctor.prototype.getDoctor = function(medicalIssue){
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response){
+    $('.showDoctors').append("<li>" + response.profile + "</li>");
+  }).fail(function(error){
+    $('.showDoctors').text(error.responseJSON.message);
+  });
 }
 
 exports.doctorModule = Doctor;
