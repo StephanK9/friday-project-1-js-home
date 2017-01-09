1f07a22d080a14480b45d1cc947df3f8f7a966a1
StephanK9 <stephane.khoury@gmail.com>
2017-01-08 22:17:00 -0800

    add working api retrieving lists of doctors, used jsonprettyprint to analyze the return value

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
   $('#searchDoctor').click(function(){
     var medicalIssue = $('#symptom').val();
     $('#symptom').val("");
-    $('.showDoctors').text("The symptom you are experiencing is " + medicalIssue + ".");
+    $('#showDoctors').text("The symptom you are looking up is " + medicalIssue + ".");
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey, function(response){
+      console.log(JSON.stringify(response));
+    });
   });
 });
